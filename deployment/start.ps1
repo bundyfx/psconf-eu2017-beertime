04b43b136bfa4ff1e349f8b0ada6f29949613159
Flynn Bundy <f.bundy@coolblue.nl>
2017-02-11 17:43:29 +0100

    t

--- deployment/start.ps1
+++ deployment/start.ps1
@@ -1,3 +1,3 @@
 Set-Location C:\App
 Start-Process 'C:\Program Files\nodejs\npm.cmd' -argumentlist 'install' -Wait
-Start-Process 'C:\Program Files\nodejs\npm.cmd' -argumentlist 'start'
+Start-Process 'C:\Program Files\nodejs\node.exe' -argumentlist 'C:\App\index.js'
