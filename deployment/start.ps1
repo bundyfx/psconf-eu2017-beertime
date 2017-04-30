22b0869cc5d2ee0f33c188b573b54ee6ffbc325c
Flynn Bundy <flynn.sbundy@gmail.com>
2017-04-30 18:05:32 +0200

    Update start.ps1

--- deployment/start.ps1
+++ deployment/start.ps1
@@ -1,3 +1,4 @@
 Set-Location C:\App
+refreshenv
 Start-Process 'C:\Program Files\nodejs\npm.cmd' -argumentlist 'install' -Wait
 Start-Process 'C:\Program Files\nodejs\node.exe' -argumentlist 'C:\App\index.js'
